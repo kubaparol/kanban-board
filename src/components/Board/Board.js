1e018eb6bd8521f97a7c7c19060704fdb3be2592
Jakub Parol <kubciop33@gmail.com>
2022-06-08 16:11:32 +0200

    feat(App): add Column compoment

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 import classes from './Board.module.css';
 
+import Column from '../Column';
+
 const Board = function () {
     return (
         <section className={classes.root}>
             <h2>Board</h2>
+            <Column />
+            <Column />
+            <Column />
+            <Column />
+            <Column />
         </section>
     );
 };
---
+++ src/components/Column/Column.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Column.module.css';
+
+const Column = function () {
+    return (
+        <article className={classes.root}>
+            <h2>Column</h2>
+        </article>
+    );
+};
+
+export default Column;
