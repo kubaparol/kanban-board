ab27fb906ce8b1dac83146777d5ec653b98d7102
Jakub Parol <kubciop33@gmail.com>
2022-06-09 19:40:16 +0200

    feat(hooks): add custom hooks

---
+++ src/hooks.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export const useChangeHandler = () => {
+    const [value, setValue] = useState('');
+
+    return {
+        value,
+        onChange: (e) => setValue(e.target.value),
+        clear: () => setValue(''),
+    };
+};
+
+export const useStorage = () => {
+    const getItem = () => JSON.parse(window.localStorage.getItem('tasks'));
+
+    const setItem = (tasks) => window.localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    return [getItem, setItem];
+};
